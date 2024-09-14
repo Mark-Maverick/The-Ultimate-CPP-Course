fa391a784279c8394f4283b4593186e41c03d30a
Mark-Maverick <developer.markmaverick@gmail.com>
2024-09-14 16:09:14 +0500

    Added code of Video 03

---
+++ VIdeo 03/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    n = 23; 
+    cout << "n = "<< n << endl;
+    
+    int a = 25;
+    // cout << "hello world" << endl;
+    cout << "a = " << a << endl;
+    float b = 1.25;
+    cout << sizeof(b) << endl;
+    cout << "b = " << b << endl;
+    bool c = false; // in boolean algebra true is 1 and false is 0
+    cout << "c = " << c << endl;
+    char d = 'M';
+    cout << "d = " << d << endl;
+    double e = 0.12345;
+    cout << "e = " << e;
+    return 0;
+}
